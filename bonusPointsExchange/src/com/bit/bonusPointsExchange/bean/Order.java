1fee65192d550be4945c72eef25945edc8b83ba8
CarlosDGQ <1255826318@qq.com>
2016-10-06 17:10:00 +0800

    加import util.Date

--- bonusPointsExchange/src/com/bit/bonusPointsExchange/bean/Order.java
+++ bonusPointsExchange/src/com/bit/bonusPointsExchange/bean/Order.java
@@ -1,6 +1,6 @@
 package com.bit.bonusPointsExchange.bean;
 
-
+import java.util.Date;
 
 /**
  * 订单
@@ -8,7 +8,7 @@ package com.bit.bonusPointsExchange.bean;
  */
 
 public class Order {
-	
+
 	private int orderID;//订单号
 	private int point;//交易积分
 	private String userName;//积分所属商家名
